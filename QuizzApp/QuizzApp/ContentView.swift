70b6ea00949a84851c28bd86608cf9e57ab46171
RyanL72 <105567147+RyanL72@users.noreply.github.com>
2025-03-15 09:40:19 -0400

    Multi View Dev

---
+++ QuizzApp/QuizzApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuizzApp
+//
+//  Created by Ryan Lin on 3/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuizzApp/QuizzApp/QuizzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzAppApp.swift
+//  QuizzApp
+//
+//  Created by Ryan Lin on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct QuizzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
